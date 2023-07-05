bf9fd10ec1cddfd408906cbe03643b6e264278c2
gabrielPnunes <95189995+gabrielPnunes@users.noreply.github.com>
2023-07-04 21:23:41 -0300

    Add files via upload

---
+++ aulas/date.js
@@ -0,0 +1,12 @@
+var d =  new Date("05-14-2022 15:30:55:36");
+var dias = ["dom", "seg", "ter", "qua", "qui", "sex", "sab"] 
+
+console.log(d)
+console.log(d.getFullYear())
+console.log(d.getMonth())
+console.log(d.getDate())
+console.log(dias[d.getDay()])
+console.log(d.getHours())
+console.log(d.getMinutes())
+console.log(d.getSeconds())
+console.log(d.getMilliseconds())
